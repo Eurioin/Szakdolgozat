7dc516492447ebebf5b25a64de0fcfd810027ed7
klambi <klricsi98@gmail.com>
2020-12-02 18:52:22 +0100

    service base for mongo connection

---
+++ WebApplication1/Services/Interfaces/AbstractServiceBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Services.Interfaces
+{
+    public abstract class AbstractServiceBase<T> : IMongoCrudRepository<T>
+    {
+        public void Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByProperty(string propertyname, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(string id, T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApplication1/Services/Interfaces/IMongoCrudRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Services.Interfaces
+{
+    public interface IMongoCrudRepository<T>
+    {
+        T GetById(string id);
+
+        T GetByProperty(string propertyname, string value);
+
+        List<T> GetAll();
+
+        void Create(T item);
+
+        void Update(string id, T item);
+
+        void Remove(T item);
+
+        void Remove(string id);
+    }
+}
