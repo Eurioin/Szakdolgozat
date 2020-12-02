a77a4e358176e95744f2a8f417964adf2b63505d
klambi <klricsi98@gmail.com>
2020-12-02 18:52:10 +0100

    Empty controllers for api calls

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        public AccountController()
+        {
+            // repo kezelő ide
+        }
+
+        // Get users informations by id (GPDR safe probably)
+        public ActionResult Details(string userid)
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ProjectController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class ProjectController : Controller
+    {
+        public ProjectController()
+        {
+            // repo kezelő ide
+        }
+
+        public ActionResult Details(string projectId)
+        {
+            // részletek lekérése
+            return null;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            // létrehozás
+            return null
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            // szerkesztés
+            return null;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(string projectId)
+        {
+            // törlés
+            return null;
+        }
+    }
+}
